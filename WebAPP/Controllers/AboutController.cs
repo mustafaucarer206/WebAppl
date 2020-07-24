b1194ae21805da6a10dfa764a399021e2e87a348
mustafaucarer206 <62465865+mustafaucarer206@users.noreply.github.com>
2020-07-24 15:48:12 +0300

    blog sayfaları

---
+++ WebAPP/Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAPP.Models.Sınıflar;
+
+namespace WebAPP.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: Abaut
+        Context snf = new Context();
+        
+        public ActionResult Index()
+        {
+            var degerler = snf.Hakkimisdas.ToList();
+            return View(degerler);
+        }
+    }
+}
---
+++ WebAPP/Controllers/BlogController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAPP.Models.Sınıflar;
+
+namespace WebAPP.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var bloglar = c.Blogs.ToList();
+            return View(bloglar);
+        }
+        public ActionResult BlogDetay(int id)
+        {
+            var blogbul = c.Blogs.Where(x => x.ID == id).ToList();
+            return View(blogbul);
+        }
+    }
+}
